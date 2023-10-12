71de6563482843c286404ef3195778e3fe2c6a68
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-10-12 18:40:06 +0300

    create project

---
+++ src/main/java/hu/progmatic/lordofthesprings/LordOfTheSpringsApplication.java
@@ -0,0 +1,13 @@
+package hu.progmatic.lordofthesprings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LordOfTheSpringsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LordOfTheSpringsApplication.class, args);
+    }
+
+}
